0cfcdc699ba2efb9a5d7252ee1e191308a9c7d25
KarolKow294 <kowalski.karol294@gmail.com>
2024-01-02 22:17:48 +0100

    Email actions

--- src/com/barosanu/EmailManager.java
+++ src/com/barosanu/EmailManager.java
@@ -1,21 +1,87 @@
 package com.barosanu;
 
 import com.barosanu.controller.services.FetchFoldersService;
+import com.barosanu.controller.services.FolderUpdaterService;
 import com.barosanu.model.EmailAccount;
+import com.barosanu.model.EmailMessage;
 import com.barosanu.model.EmailTreeItem;
 import javafx.scene.control.TreeItem;
 
+import javax.mail.Flags;
+import javax.mail.Folder;
+import javax.mail.MessagingException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmailManager {
+    private EmailMessage selectedMessage;
+    private EmailTreeItem<String> selectedFolder;
+    private FolderUpdaterService folderUpdaterService;
     private EmailTreeItem<String> foldersRoot = new EmailTreeItem<>("");
+
+    public EmailMessage getSelectedMessage() {
+        return selectedMessage;
+    }
+
+    public void setSelectedMessage(EmailMessage selectedMessage) {
+        this.selectedMessage = selectedMessage;
+    }
+
+    public EmailTreeItem<String> getSelectedFolder() {
+        return selectedFolder;
+    }
+
+    public void setSelectedFolder(EmailTreeItem<String> selectedFolder) {
+        this.selectedFolder = selectedFolder;
+    }
 
     public EmailTreeItem<String> getFoldersRoot() {
         return foldersRoot;
     }
 
+    private List<Folder> folderList = new ArrayList<Folder>();
+    public List<Folder> getFolderList() {
+        return this.folderList;
+    }
+
+    public EmailManager() {
+        folderUpdaterService = new FolderUpdaterService(folderList);
+        folderUpdaterService.start();
+    }
+
     public void addEmailAccount(EmailAccount emailAccount) {
         EmailTreeItem<String> treeItem = new EmailTreeItem<>(emailAccount.getAddress());
-        FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem);
+        FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem, folderList);
         fetchFoldersService.start();
         foldersRoot.getChildren().add(treeItem);
+    }
+
+    public void setRead() {
+        try {
+            selectedMessage.setRead(true);
+            selectedMessage.getMessage().setFlag(Flags.Flag.SEEN, true);
+            selectedFolder.decrementMessagesCount();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setUnRead() {
+        try {
+            selectedMessage.setRead(false);
+            selectedMessage.getMessage().setFlag(Flags.Flag.SEEN, false);
+            selectedFolder.incrementMessagesCount();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteSelectedMessage() {
+        try {
+            selectedMessage.getMessage().setFlag(Flags.Flag.DELETED, false);
+            selectedFolder.getEmailMessages().remove(selectedMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
